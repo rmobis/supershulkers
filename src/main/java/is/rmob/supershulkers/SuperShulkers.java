c6f8d163a71649523490ebf4d7df2f087ae63047
Raphael Mobis Tacla <r.mobis@gmail.com>
2020-12-24 05:26:45 -0300

    automatically register custom scarpet script as a builtin module

--- src/main/java/is/rmob/supershulkers/SuperShulkers.java
+++ src/main/java/is/rmob/supershulkers/SuperShulkers.java
@@ -1,5 +1,14 @@
 package is.rmob.supershulkers;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import carpet.script.CarpetScriptServer;
+import carpet.script.bundled.BundledModule;
 import is.rmob.supershulkers.enchantments.EnlargeEnchantment;
 import is.rmob.supershulkers.enchantments.RestockEnchantment;
 import is.rmob.supershulkers.enchantments.VacuumEnchantment;
@@ -8,24 +17,44 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class SuperShulkers implements ModInitializer {
+	private static final Logger LOGGER = LogManager.getLogger(SuperShulkers.class);
+
 	@Override
 	public void onInitialize() {
-		Registry.register(
+		this.registerEnchantments();
+		this.registerScarpetApp();
+	}
+
+	private void registerEnchantments() {
+		EnlargeEnchantment ENLARGE_ENCHANTMENT = Registry.register(
 			Registry.ENCHANTMENT,
 			new Identifier("supershulkers", "enlarge"),
 			new EnlargeEnchantment()
 		);
 
-		Registry.register(
+		LOGGER.info("Registered enchantment {}", ENLARGE_ENCHANTMENT);
+
+		RestockEnchantment RESTOCK_ENCHANTMENT = Registry.register(
 			Registry.ENCHANTMENT,
 			new Identifier("supershulkers", "restock"),
 			new RestockEnchantment()
 		);
 
-		Registry.register(
+		LOGGER.info("Registered enchantment {}", RESTOCK_ENCHANTMENT);
+
+		VacuumEnchantment VACUUM_ENCHANTMENT = Registry.register(
 			Registry.ENCHANTMENT,
 			new Identifier("supershulkers", "vacuum"),
 			new VacuumEnchantment()
 		);
+
+		LOGGER.info("Registered enchantment {}", VACUUM_ENCHANTMENT);
+	}
+
+	private void registerScarpetApp() {
+		BundledModule superShulkersApp = BundledModule.fromPath("assets/supershulkers/scripts/", "supershulkers", false);
+		CarpetScriptServer.registerBuiltInScript(superShulkersApp);
+
+		LOGGER.info("Registered scarpet app {}", superShulkersApp);
 	}
 }
